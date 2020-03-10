d27b21b03af75f95c4887bf31d99fad380257709
Colby B <colby.birrell@gmail.com>
2020-03-10 12:39:21 -0600

    savingform

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,5 +1,41 @@
 import React from "react";
+// import { connect } from "react-redux";
+// import axios from "axios";
+import { Link } from "react-router-dom";
+import "./form.css";
 
-export default function Form() {
-  return <div>Form.js</div>;
+function Form() {
+  return (
+    <div className="form-body">
+      <div className="form-main">
+        Create a New Post
+        <img className="img-form" alt="" />
+        <div className="form-inputs">
+          <input
+            name="postTitle"
+            placeholder="Enter Post Title"
+            // onChange={this.handleInput}
+          />
+          <textarea
+            name="postText"
+            placeholder="Enter Post Text"
+            // onChange={this.handleInput}
+          />
+          <input
+            name="img"
+            placeholder="Enter img URL"
+            // onChange={this.handleInput}
+          />
+        </div>
+        <div className="form-butts">
+          <Link to="/dashboard">
+            <button>List It</button>
+          </Link>
+          <button>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Form;
