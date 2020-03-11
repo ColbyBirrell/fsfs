b8a65ed45566093e714d1ce5b18f194a0aa51f10
Colby B <colby.birrell@gmail.com>
2020-03-11 17:58:54 -0600

    saving

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import Post from "../post/Post";
+// import axios from "axios";
 
-export default function Posts(props) {
-  return <div>Posts Comp</div>;
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      postList: []
+    };
+  }
+
+  // componentDidMount() {
+  //   this.getPosts();
+  // }
+
+  // getPosts = (req, res) => {
+  //   axios
+  //     .get(`/api/posts`)
+  //     .then(res => {
+  //       this.setState({
+  //         postList: res.data
+  //       });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
+
+  render() {
+    return <div>Posts Component</div>;
+  }
 }
+
+const mapStateToProps = reduxState => {
+  return {
+    userReducer: reduxState.userReducer
+  };
+};
+
+export default connect(mapStateToProps, {})(Posts);
